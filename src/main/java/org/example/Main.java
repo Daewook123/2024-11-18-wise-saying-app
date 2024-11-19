a37157bb987dfe2bd82bc2f6c32b3be82a6e8778
Daewook123 <cdo5080@gmail.com>
2024-11-19 18:48:34 +0900

    9단계 완료(File로 CRUD)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,18 @@
 package org.example;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) {
 
-        List<Writer> list = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
+
+        String path = "src/main/java/org/example/db/wiseSaying";
 
         System.out.println("==명언 앱==");
 
@@ -19,21 +22,21 @@ public class Main {
 
             switch (command){
                 case "등록":
-                    list.add(create(sc));
-                    System.out.println(list.size()+"번 명언이 등록되었습니다.");
+                    create(sc, path);
                     break;
                 case "목록":
-                    read(list);
+                    read(path);
                     break;
                 case "삭제":
                     System.out.print("(첫 인덱스는 1 입니다)?id= ");
-                    delete(list, sc);
+                    delete(sc, path);
                     break;
                 case "수정":
                     System.out.print("(첫 인덱스는 1 입니다)?id= ");
-                    update(list, sc);
+                    update(sc, path);
                     break;
                 case "종료":
+                    sc.close();
                     return;
                 default:
                     continue;
@@ -42,57 +45,171 @@ public class Main {
         }
     }
 
-    public static Writer create(Scanner sc) {
-
-        Writer wt = new Writer();
+    public static void create(Scanner sc, String path) {
 
         System.out.print("명언 : ");
         String text = sc.nextLine();
-        wt.setSaying(text);
 
         System.out.print("작가 : ");
         String writer = sc.nextLine();
-        wt.setWriter(writer);
 
-        return wt;
+        String lastIdPath = path + "/lastId.txt";
+        int count = lastIdFileRead(lastIdPath);
+
+        String writerFilePath = path + "/" + count + ".json";
+        writerFileCreate(writerFilePath, count, text, writer);
+
+        System.out.println(count + "번 명언이 등록되었습니다.");
+
     }
 
-    public static void read(List<Writer> list){
-        System.out.println("번호 / 작가 / 명언");
-        System.out.println("----------------------");
-        for(int i=list.size()-1; i>=0; i--){
-            System.out.println(i+1+" / "+list.get(i).getWriter()+" / "+list.get(i).getSaying());
+    public static int lastIdFileRead(String lastIdPath) {
+        try {
+
+            File file = new File(lastIdPath);
+            BufferedReader reader = null;
+
+            if (!file.exists()) {
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+
+                try (FileWriter filewriter = new FileWriter(file)) {
+                    filewriter.write("0");
+                }
+                file = new File(lastIdPath);
+            }
+
+            reader = new BufferedReader(new FileReader(file));
+            int count = Integer.parseInt(reader.readLine()) + 1;
+
+            try (FileWriter filewriter = new FileWriter(file)) {
+                filewriter.write(String.valueOf(count));
+            }
+
+            return count;
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return 0;
         }
     }
 
-    public static void delete(List<Writer> list, Scanner sc){
+    public static void writerFileCreate(String writerFilePath, int count, String text, String writer) {
+        try {
 
-        int index = sc.nextInt(); // Integer.ParseInt(sc.nextLine()); <- 사용시 다음 행 X
-        sc.nextLine(); // enter 삭제
+            File file = new File(writerFilePath);
+
+            file.getParentFile().mkdirs();
+            file.createNewFile();
+
+            try (FileWriter filewriter = new FileWriter(file)) {
+                String jsonString = String.format("{\"id\":\"%d\",\"content\":\"%s\",\"author\":\"%s\"}", count, text, writer);
+                filewriter.write(jsonString);
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void read(String path){
 
         try {
-            list.remove(index-1);
-            System.out.println(index+"번 명언이 삭제되었습니다.");
-        }catch (Exception e) {
-            System.out.println(index+"번 명언은 존재하지 않습니다.");
+            String lastIdPath = path + "/lastId.txt";
+
+            File lastIdfile = new File(lastIdPath);
+            BufferedReader reader = null;
+
+            if(!lastIdfile.exists()) {
+                System.out.println("명언이 존재하지 않습니다.");
+                return;
+            }
+
+            reader = new BufferedReader(new FileReader(lastIdfile));
+            int count = Integer.parseInt(reader.readLine());
+
+            System.out.println("번호 / 작가 / 명언");
+            System.out.println("----------------------");
+
+            for(int i = 1; i<=count; i++){
+                String writerFilePath = path + "/" + i + ".json";
+                File writerFile = new File(writerFilePath);
+                if(!writerFile.exists()){
+                    continue;
+                }else {
+                    reader = new BufferedReader(new FileReader(writerFile));
+                    String list = reader.readLine().replace("{", "").replace("\"", "").replace("}", "");
+                    String[] arr = list.split(":|,");
+                    System.out.println(arr[1] + " / " + arr[3] + " / " + arr[5]);
+                }
+            }
+
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
         }
     }
 
-    public static void update(List<Writer> list, Scanner sc){
+    public static void delete(Scanner sc, String path){
 
         int index = sc.nextInt(); // Integer.ParseInt(sc.nextLine()); <- 사용시 다음 행 X
         sc.nextLine(); // enter 삭제
 
-        System.out.println("명언(기존) : "+list.get(index-1).getSaying());
-        System.out.print("명언 : ");
-        String text = sc.nextLine();
-        list.get(index-1).setSaying(text);
+        String writerFilePath = path + "/" + index + ".json";
 
-        System.out.println("작가(기존) : "+list.get(index-1).getWriter());
-        System.out.print("작가 : ");
-        String writer = sc.nextLine();
-        list.get(index-1).setWriter(writer);
+        File file = new File(writerFilePath);
+
+        if (file.exists()) {
+            boolean deleted = file.delete();
+            if (deleted) {
+                System.out.println(index+"번 명언이 삭제되었습니다.");
+            } else {
+                System.out.println(index+"번 명언 삭제를 실패하였습니다:");
+            }
+        } else {
+            System.out.println(index + "번 명언은 존재하지 않습니다.");
+        }
+    }
+
+
+    public static void update(Scanner sc, String path){
+
+        try {
+            int index = sc.nextInt(); // Integer.ParseInt(sc.nextLine()); <- 사용시 다음 행 X
+            sc.nextLine(); // enter 삭제
+
+            String writerFilePath = path + "/" + index + ".json";
+
+            File writerFile = new File(writerFilePath);
+            BufferedReader reader = null;
+
+            if (!writerFile.exists()) {
+                System.out.println(index + "번 명언은 존재하지 않습니다.");
+            }else{
+                reader = new BufferedReader(new FileReader(writerFile));
+                String list = reader.readLine().replace("{", "").replace("\"", "").replace("}", "");
+                String[] arr = list.split(":|,");
+
+                System.out.println("명언(기존) : "+ arr[3]);
+                System.out.print("명언 : ");
+                String text = sc.nextLine();
+
+                System.out.println("작가(기존) : "+ arr[5]);
+                System.out.print("작가 : ");
+                String writer = sc.nextLine();
+
+                writerFileCreate(writerFilePath, index, text, writer);
+            }
+
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
 
     }
-    
+
 }
+
+
+    
+
--- src/main/java/org/example/Writer.java
+++ src/main/java/org/example/Writer.java
@@ -1,27 +1,32 @@
-package org.example;
-
-public class Writer {
-
-    public Writer() {
-    }
-
-    private String saying;
-
-    private String writer;
-
-    public String getSaying() {
-        return saying;
-    }
-
-    public void setSaying(String saying) {
-        this.saying = saying;
-    }
-
-    public String getWriter() {
-        return writer;
-    }
-
-    public void setWriter(String writer) {
-        this.writer = writer;
-    }
-}
+//package org.example;
+//
+//import java.util.concurrent.atomic.AtomicInteger;
+//
+//public class Writer {
+//
+//    private String saying;
+//
+//    private String writer;
+//
+//    public Writer(String saying, String writer) {
+//
+//        this.saying = saying;
+//        this.writer = writer;
+//    }
+//
+//    public String getSaying() {
+//        return saying;
+//    }
+//
+//    public void setSaying(String saying) {
+//        this.saying = saying;
+//    }
+//
+//    public String getWriter() {
+//        return writer;
+//    }
+//
+//    public void setWriter(String writer) {
+//        this.writer = writer;
+//    }
+//}
