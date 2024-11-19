1a13d268f54d83f9a7622ab34bb1a5924721e2a3
Daewook123 <cdo5080@gmail.com>
2024-11-19 11:48:38 +0900

    8단계까지 완성

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,98 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Writer> list = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("==명언 앱==");
+
+        while (true) {
+            System.out.print("명령)");
+            String command = sc.nextLine();
+
+            switch (command){
+                case "등록":
+                    list.add(create(sc));
+                    System.out.println(list.size()+"번 명언이 등록되었습니다.");
+                    break;
+                case "목록":
+                    read(list);
+                    break;
+                case "삭제":
+                    System.out.print("(첫 인덱스는 1 입니다)?id= ");
+                    delete(list, sc);
+                    break;
+                case "수정":
+                    System.out.print("(첫 인덱스는 1 입니다)?id= ");
+                    update(list, sc);
+                    break;
+                case "종료":
+                    return;
+                default:
+                    continue;
+            }
+
         }
     }
+
+    public static Writer create(Scanner sc) {
+
+        Writer wt = new Writer();
+
+        System.out.print("명언 : ");
+        String text = sc.nextLine();
+        wt.setSaying(text);
+
+        System.out.print("작가 : ");
+        String writer = sc.nextLine();
+        wt.setWriter(writer);
+
+        return wt;
+    }
+
+    public static void read(List<Writer> list){
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+        for(int i=list.size()-1; i>=0; i--){
+            System.out.println(i+1+" / "+list.get(i).getWriter()+" / "+list.get(i).getSaying());
+        }
+    }
+
+    public static void delete(List<Writer> list, Scanner sc){
+
+        int index = sc.nextInt(); // Integer.ParseInt(sc.nextLine()); <- 사용시 다음 행 X
+        sc.nextLine(); // enter 삭제
+
+        try {
+            list.remove(index-1);
+            System.out.println(index+"번 명언이 삭제되었습니다.");
+        }catch (Exception e) {
+            System.out.println(index+"번 명언은 존재하지 않습니다.");
+        }
+    }
+
+    public static void update(List<Writer> list, Scanner sc){
+
+        int index = sc.nextInt(); // Integer.ParseInt(sc.nextLine()); <- 사용시 다음 행 X
+        sc.nextLine(); // enter 삭제
+
+        System.out.println("명언(기존) : "+list.get(index-1).getSaying());
+        System.out.print("명언 : ");
+        String text = sc.nextLine();
+        list.get(index-1).setSaying(text);
+
+        System.out.println("작가(기존) : "+list.get(index-1).getWriter());
+        System.out.print("작가 : ");
+        String writer = sc.nextLine();
+        list.get(index-1).setWriter(writer);
+
+    }
+    
 }
---
+++ src/main/java/org/example/Writer.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Writer {
+
+    public Writer() {
+    }
+
+    private String saying;
+
+    private String writer;
+
+    public String getSaying() {
+        return saying;
+    }
+
+    public void setSaying(String saying) {
+        this.saying = saying;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+}
